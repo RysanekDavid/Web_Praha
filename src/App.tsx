82ea4957cd55b897a5a898fabe590dc5c6c07d51
DavidRysanek <dawelich@gmail.com>
2023-09-17 11:43:32 +0200

    Responsivity fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import './App.css';
-import ResponsiveAppBar from "./components/AppBar"
-import ContentBox from './components/Banner';
-
-
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import "./App.css";
+import ResponsiveAppBar from "./components/AppBar";
+import ContentBox from "./components/Banner";
 
 const customTheme = createTheme({
   breakpoints: {
     values: {
       xs: 0,
-      sm: 650,
+      sm: 700,
       md: 900,
       lg: 1388, // Upravena hodnota
       xl: 1536,
@@ -18,13 +16,12 @@ const customTheme = createTheme({
   },
 });
 
-
 function App() {
   return (
-  <ThemeProvider theme={customTheme}>
-    <ResponsiveAppBar />
-    <ContentBox />
-  </ThemeProvider > 
+    <ThemeProvider theme={customTheme}>
+      <ResponsiveAppBar />
+      <ContentBox />
+    </ThemeProvider>
   );
 }
 
--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,45 +1,51 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import logo from "../images/Portál-hlavního-města-Prahy.png"
-import SearchBar from './SearchBar';
-import MenuIcon from '@mui/icons-material/Menu';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import logo from "../images/Portál-hlavního-města-Prahy.png";
+import SearchBar from "./SearchBar";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
 import { useTranslation } from "react-i18next";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import LanguageIcon from '@mui/icons-material/Language';
-import VideocamIcon from '@mui/icons-material/Videocam';
-import CallIcon from '@mui/icons-material/Call';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import EmailIcon from '@mui/icons-material/Email';
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import MenuBookIcon from '@mui/icons-material/MenuBook';
-import WeatherWidget from './WeatherDisplay';
-import "../styles/LogoSize.css"
-import { Grid, Hidden } from '@mui/material';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import LanguageIcon from "@mui/icons-material/Language";
+import VideocamIcon from "@mui/icons-material/Videocam";
+import CallIcon from "@mui/icons-material/Call";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import EmailIcon from "@mui/icons-material/Email";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import MenuBookIcon from "@mui/icons-material/MenuBook";
+import WeatherWidget from "./WeatherDisplay";
+import "../styles/LogoSize.css";
+import { Grid, Hidden } from "@mui/material";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
-
-
-
-const pages = ['O městě', 'Potřebuji řešit','Doprava','Co dělat v Praze', 'Kontakty'];
-
+const pages = [
+  "O městě",
+  "Potřebuji řešit",
+  "Doprava",
+  "Co dělat v Praze",
+  "Kontakty",
+];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
- 
-  
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+
   const theme = useTheme();
-  const isMobile = useMediaQuery(theme.breakpoints.down('lg'));
-  const isSmallScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const isMobile = useMediaQuery(theme.breakpoints.down("lg"));
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -49,10 +55,10 @@ function ResponsiveAppBar() {
     setAnchorElNav(null);
   };
 
-
   const { i18n } = useTranslation();
 
-  const [languageMenuAnchor, setLanguageMenuAnchor] = React.useState<null | HTMLElement>(null);
+  const [languageMenuAnchor, setLanguageMenuAnchor] =
+    React.useState<null | HTMLElement>(null);
 
   const switchLanguage = (lng: string) => {
     i18n.changeLanguage(lng);
@@ -63,41 +69,60 @@ function ResponsiveAppBar() {
     setLanguageMenuAnchor(event.currentTarget);
   };
 
-  const closeLanguageMenu = () => { 
+  const closeLanguageMenu = () => {
     setLanguageMenuAnchor(null);
   };
 
-
   return (
-    <AppBar position="static" sx={{ background: '#FFFFFF', paddingTop: isSmallScreen ? '10px' : '0px' }}>
+    <AppBar
+      position="static"
+      sx={{ background: "#FFFFFF", paddingTop: isSmallScreen ? "10px" : "0px" }}
+    >
       <Container maxWidth="xl">
-        <Toolbar disableGutters sx={{justifyContent: 'center', alignItems: 'center'}}>
-        <img src={logo} alt="Logo" className="logo"  />
-          <Box sx={{ flexGrow:0.95, display:'flex'}}>
+        <Toolbar
+          disableGutters
+          sx={{ justifyContent: "center", alignItems: "center" }}
+        >
+          <img src={logo} alt="Logo" className="logo" />
+          <Box sx={{ flexGrow: 0.95, display: "flex" }}>
             <SearchBar />
           </Box>
-          
-          
 
           {isMobile ? (
             <>
-              <IconButton edge="start" color="inherit" aria-label="menu" onClick={handleOpenNavMenu}>
-                <MenuIcon style={{color: "black"}}/>
+              <IconButton
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleOpenNavMenu}
+              >
+                <MenuIcon style={{ color: "black" }} />
               </IconButton>
               <Hidden mdDown>
-                <Typography variant="h5" component="div" sx={{ color:'black', flexGrow: 1, marginLeft: "10px", whiteSpace: "nowrap" }}>Hlavní menu</Typography>
+                <Typography
+                  variant="h5"
+                  component="div"
+                  sx={{
+                    color: "black",
+                    flexGrow: 1,
+                    marginLeft: "10px",
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  Hlavní menu
+                </Typography>
               </Hidden>
               <Menu
                 id="menu-appbar"
                 anchorEl={anchorElNav}
                 anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'left',
+                  vertical: "bottom",
+                  horizontal: "left",
                 }}
                 keepMounted
                 transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'left',
+                  vertical: "top",
+                  horizontal: "left",
                 }}
                 open={Boolean(anchorElNav)}
                 onClose={handleCloseNavMenu}
@@ -110,124 +135,306 @@ function ResponsiveAppBar() {
               </Menu>
             </>
           ) : (
-            <Box sx={{ flexGrow: 5, display: 'flex', alignItems: 'center', paddingTop: "2px"}}>
+            <Box
+              sx={{
+                flexGrow: 5,
+                display: "flex",
+                alignItems: "center",
+                paddingTop: "2px",
+              }}
+            >
               {pages.map((page) => (
                 <Button
                   key={page}
                   onClick={handleCloseNavMenu}
-                  sx={{ my:2, color: '#45494f', display: 'block', fontWeight: "550", paddingLeft : "30px", whiteSpace: "nowrap", fontSize: "15px"}}
+                  sx={{
+                    my: 2,
+                    color: "#45494f",
+                    display: "block",
+                    fontWeight: "550",
+                    paddingLeft: "30px",
+                    whiteSpace: "nowrap",
+                    fontSize: "15px",
+                  }}
                 >
                   {page}
                 </Button>
               ))}
             </Box>
-
           )}
 
-          <Box sx={{ flexGrow: 0, color: "black", marginRight: "0px" , marginLeft: "30px", display: 'flex', alignItems: 'center'}}>
+          <Box
+            sx={{
+              flexGrow: 0,
+              color: "black",
+              marginRight: "0px",
+              marginLeft: "30px",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <VideocamIcon
+              style={{
+                fontSize: isSmallScreen ? "24px" : "28px",
+                paddingTop: "5px",
+              }}
+            />
 
-            <VideocamIcon style = {{ fontSize:isSmallScreen ? "24px" : "28px", paddingTop: "5px" }} />
+            <Box
+              sx={{
+                height: "30px",
+                width: "1px",
+                backgroundColor: "black",
+                marginRight: "12px",
+                marginLeft: "12px",
+              }}
+            />
 
-          <Box sx={{ height: "30px", width: "1px", backgroundColor: "black", marginRight: "12px", marginLeft: "12px"}} />
-          
-          <WeatherWidget />
-
+            <WeatherWidget />
           </Box>
 
-          <Box sx={{ flexGrow: 0, display: 'flex', alignItems: 'center', marginRight: "5px"}}>
+          <Box
+            sx={{
+              flexGrow: 0,
+              display: "flex",
+              alignItems: "center",
+              marginRight: "5px",
+            }}
+          >
             {/* Přepínač jazyků */}
-            <Button 
-                onClick={openLanguageMenu} 
-                startIcon={<LanguageIcon style={{ color: "black", fontSize: "25px" }} />} 
-                endIcon={<KeyboardArrowDownIcon style={{ color: "black", fontSize: "25px" }} />}  
-                style={{ color: "black" , fontSize: "16px"}}>
-                <b>{i18n.language === 'cs' ? 'EN' : 'CZ'}</b>
+            <Button
+              onClick={openLanguageMenu}
+              startIcon={
+                <LanguageIcon style={{ color: "black", fontSize: "25px" }} />
+              }
+              endIcon={
+                <KeyboardArrowDownIcon
+                  style={{ color: "black", fontSize: "25px" }}
+                />
+              }
+              style={{ color: "black", fontSize: "16px" }}
+            >
+              <b>{i18n.language === "cs" ? "EN" : "CZ"}</b>
             </Button>
-            <Menu 
+            <Menu
               anchorEl={languageMenuAnchor}
               keepMounted
               open={Boolean(languageMenuAnchor)}
               onClose={closeLanguageMenu}
             >
-              <MenuItem onClick={() => switchLanguage('en')}>EN</MenuItem>
-              <MenuItem onClick={() => switchLanguage('cs')}>CZ</MenuItem>
-              
+              <MenuItem onClick={() => switchLanguage("en")}>EN</MenuItem>
+              <MenuItem onClick={() => switchLanguage("cs")}>CZ</MenuItem>
             </Menu>
           </Box>
-
-
-          
         </Toolbar>
       </Container>
-      
-      <Box sx={{ backgroundColor: '#413D3C', color: 'white', paddingBottom: '3px',paddingTop: '3px', whiteSpace: 'nowrap'}}>
-      <Grid container spacing={0} justifyContent="space-between" sx={{flexWrap: 'wrap', paddingLeft: '6%', paddingRight: '10%'}}>
 
-        <Grid item xs={1} sm={1} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center'}}>
-          <Tooltip title="800 100 000">
-            <CallIcon sx={{width: '22px', height: '22px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden mdDown>
-            800 100 000
+      <Box
+        sx={{
+          backgroundColor: "#413D3C",
+          color: "white",
+          paddingBottom: "3px",
+          paddingTop: "3px",
+          whiteSpace: "nowrap",
+        }}
+      >
+        <Grid
+          container
+          spacing={0}
+          justifyContent="space-between"
+          sx={{ flexWrap: "wrap", paddingLeft: "", paddingRight: "10%" }}
+        >
+          <Hidden lgUp>
+            <Accordion
+              sx={{
+                width: "100%",
+                backgroundColor: "transparent",
+                boxShadow: "none",
+              }}
+            >
+              <AccordionSummary
+                expandIcon={
+                  <ExpandMoreIcon
+                    sx={{
+                      color: "white",
+                      paddingRight: "12%",
+                    }}
+                  />
+                }
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                sx={{ color: "white", paddingLeft: "10%" }}
+              >
+                Kontakty
+              </AccordionSummary>
+              <Grid
+                container
+                spacing={0}
+                justifyContent="start"
+                display="flex"
+                sx={{ color: "white" }}
+              >
+                {
+                  <Box>
+                    <CallIcon
+                      sx={{
+                        paddingLeft: "12px",
+                        width: "22px",
+                        height: "22px",
+                        marginRight: "5px",
+                        color: "white",
+                      }}
+                    />
+                    infolinka
+                  </Box>
+                }
+              </Grid>
+            </Accordion>
           </Hidden>
-        </Grid>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={1}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Hidden lgDown>
+              <CallIcon
+                sx={{ width: "22px", height: "22px", marginRight: "5px" }}
+              />
+              800 100 000
+            </Hidden>
+          </Grid>
 
-        <Grid item xs={1} sm={1} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center'}}>
-          <Tooltip title="236 001 111 (ústředna)">
-            <CallIcon sx={{width: '22px', height: '22px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden mdDown>
-            236 001 111 (ústředna)
-          </Hidden>
-        </Grid>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={1}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Hidden lgDown>
+              <CallIcon
+                sx={{ width: "22px", height: "22px", marginRight: "5px" }}
+              />
+              236 001 111(ústředna)
+            </Hidden>
+          </Grid>
 
-        <Grid item xs={1} sm={1} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center', marginLeft:{xs: '0px', sm: '0px', md: '72px', lg: '72px', xl: '72px'},  marginRight:{xs: '0px', sm: '0px', md: '-20px', lg: '-20px', xl: '-20px'}}}>
-          <Tooltip title="praha.eu">
-            <FacebookIcon sx={{width: '23px', height: '23px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden mdDown>
-            praha.eu
-          </Hidden>
-        </Grid>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={1}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              marginLeft: {
+                xs: "0px",
+                sm: "0px",
+                md: "72px",
+                lg: "72px",
+                xl: "72px",
+              },
+              marginRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "-20px",
+                lg: "-20px",
+                xl: "-20px",
+              },
+            }}
+          >
+            <Hidden lgDown>
+              <FacebookIcon
+                sx={{ width: "23px", height: "23px", marginRight: "5px" }}
+              />
+              praha.eu
+            </Hidden>
+          </Grid>
 
-        <Grid item xs={1} sm={1} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center'}}>
-          <Tooltip title="e-podatelna">
-            <EmailIcon sx={{width: '22px', height: '22px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden mdDown>
-            e-podatelna
-          </Hidden>
-        </Grid>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={1}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Hidden lgDown>
+              <EmailIcon
+                sx={{ width: "22px", height: "22px", marginRight: "5px" }}
+              />
+              e-podatelna
+            </Hidden>
+          </Grid>
 
-        <Grid item xs={1} sm={1} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center'}}>
-          <Tooltip title="info@praha.eu">
-            <EmailIcon sx={{width: '22px', height: '22px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden mdDown>
-            info@praha.eu
-          </Hidden>
-        </Grid>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={1}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Hidden lgDown>
+              <EmailIcon
+                sx={{ width: "22px", height: "22px", marginRight: "5px" }}
+              />
+              info@praha.eu
+            </Hidden>
+          </Grid>
 
-        <Grid item xs={1} sm={1} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center'}}>
-          <Tooltip title="Portál Pražana">
-            <AccountBoxIcon sx={{width: '22px', height: '22px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden lgDown >
-            Portál Pražana
-          </Hidden>
-        </Grid>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={1}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Hidden lgDown>
+              <AccountBoxIcon
+                sx={{ width: "22px", height: "22px", marginRight: "5px" }}
+              />
+              Portál Pražana
+            </Hidden>
+          </Grid>
 
-        <Grid item xs={1} sm={2} md={2} lg={1} xl={1} sx={{ display: 'flex', alignItems: 'center'}}>
-          <Tooltip title="adresář zaměstnanců">
-            <MenuBookIcon sx={{width: '22px', height: '22px', marginRight: '5px'}} />
-          </Tooltip>
-          <Hidden lgDown>
-            adresář zaměstnanců
-          </Hidden>
+          <Grid
+            paddingLeft="6%"
+            item
+            xs={1}
+            sm={2}
+            md={2}
+            lg={1}
+            xl={1}
+            sx={{ display: "flex", alignItems: "center" }}
+          >
+            <Hidden lgDown>
+              <MenuBookIcon
+                sx={{ width: "22px", height: "22px", marginRight: "5px" }}
+              />
+              adresář zaměstnanců
+            </Hidden>
+          </Grid>
         </Grid>
-      </Grid>
-    </Box>
-
+      </Box>
     </AppBar>
   );
 }
